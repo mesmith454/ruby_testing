41909a053dc00ae0ffe2634c92d7b9272db32fc7
megan <mesmith454@gmail.com>
2024-09-18 11:40:52 -0700

    completed 06,07,08

--- spec/06_equality_spec.rb
+++ spec/06_equality_spec.rb
@@ -63,6 +63,11 @@ end
 
 # ASSIGNMENT
 
+# 'eq' checks for equal VALUE.
+# 'eql' checks for equal VALUE and TYPE.
+# 'equal' checks for OBJECT IDENTITY.
+# 'be' checks for OBJECT IDENTITY.
+
 describe 'equality assignment' do
   let(:amy) { { fav_color: 'blue', fav_food: 'tacos' } }
   let(:bob) { { fav_color: 'blue', fav_food: 'tacos' } }
@@ -70,18 +75,22 @@ describe 'equality assignment' do
   # Write a test that expresses each of the following statements.
 
   # remove the 'x' before running this test
-  xit 'amy is eq to bob' do
+  it 'amy is eq to bob' do
+    expect(amy).to eq(bob)
   end
 
   # remove the 'x' before running this test
-  xit 'amy is eql to bob' do
+  it 'amy is eql to bob' do
+    expect(amy).to eql(bob)
   end
 
   # remove the 'x' before running this test
-  xit 'amy is not equal to bob' do
+  it 'amy is not equal to bob' do
+    expect(amy).not_to equal(bob)
   end
 
   # remove the 'x' before running this test
-  xit 'copy_cat is equal to amy' do
+  it 'copy_cat is equal to amy' do
+    expect(copy_cat).to be(amy)
   end
 end
